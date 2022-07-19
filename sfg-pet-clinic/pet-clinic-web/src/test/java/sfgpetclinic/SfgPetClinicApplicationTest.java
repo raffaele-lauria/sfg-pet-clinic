aa79e3849b1282b0e4035100dc89fbc9df12bb58
Raffaele Lauria <raffaele.lauria@yahoo.it>
2022-07-19 15:52:35 +0200

    refactoring to multimodule maven project.

--- sfg-pet-clinic/pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTest.java
+++ sfg-pet-clinic/pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTest.java
@@ -1,5 +1,9 @@
 package sfgpetclinic;
 
+import guru.springframework.sfgpetclinic.SfgPetClinicApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {SfgPetClinicApplication.class})
 class SfgPetClinicApplicationTest {
 
 }
